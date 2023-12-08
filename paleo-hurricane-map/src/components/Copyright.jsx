c12dd51dde76332e496d9637f9668bd8fb0904b5
ethanandrews <eandrews@whoi.edu>
2023-12-08 14:41:16 -0500

    add temp content for about page

--- paleo-hurricane-map/src/components/Copyright.jsx
+++ paleo-hurricane-map/src/components/Copyright.jsx
@@ -5,7 +5,14 @@ import { Box } from "@mui/material";
 
 export default function Copyright() {
   return (
-    <Box sx={{ position: "absolute", bottom: 10 }}>
+    <Box
+      sx={{
+        position: "absolute",
+        bottom: 10,
+        textAlign: "center",
+        width: "100%",
+      }}
+    >
       <Typography variant="body2" color="text.secondary" align="center">
         {"Copyright © "}
         <Link color="inherit" href="https://www.whoi.edu" target="_blank">
--- paleo-hurricane-map/src/routes/about.jsx
+++ paleo-hurricane-map/src/routes/about.jsx
@@ -1,3 +1,28 @@
+import { Box, Typography } from "@mui/material";
+import Copyright from "../components/Copyright";
+
 export default function About() {
-  return <div>About page here</div>;
+  return (
+    <>
+      <Box sx={{ p: 2 }}>
+        <Typography variant="h4" gutterBottom>
+          About PaleoHurdat
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          PaleoHurdat is a project from the Woods Hole Oceanographic
+          Institution. It's purpose to make paleographic hurricane data
+          available to the public and scientific community via a map-based
+          interface.
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          Funding for this project is provided by the NSF.
+        </Typography>
+      </Box>
+      <Box sx={{ p: 2, textAlign: "center" }}>
+        <Copyright />
+      </Box>
+    </>
+  );
 }
